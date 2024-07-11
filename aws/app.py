f5d982ba91dfebc12a2dda50f01e666e977a4e2c
Alex Idowu <alexidowu25@gmail.com>
2024-07-11 10:42:12 +0100

    Update default region to "us-west-1" in app.py

--- aws/app.py
+++ aws/app.py
@@ -14,13 +14,13 @@ class AwsStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Use the default VPC
+        # Retrieve the default VPC for the specified region
         default_vpc = ec2.Vpc.from_lookup(self, "DefaultVpc", is_default=True)
 
         # Define an Amazon Linux EC2 instance
         ec2_instance = ec2.Instance(self, "MyInstance",
                                     instance_type=ec2.InstanceType("t2.micro"),
-                                    machine_image=ec2.MachineImage.latest_amazon_linux(),
+                                    machine_image=ec2.MachineImage.latest_amazon_linux2(),
                                     vpc=default_vpc)
 
         # Output the public IP of the instance
@@ -35,7 +35,7 @@ class AwsStack(Stack):
 # Initialize the CDK app
 app = App()
 account = os.getenv('CDK_DEFAULT_ACCOUNT')
-region = os.getenv('CDK_DEFAULT_REGION')
+region = "us-west-1"
 
 if not account or not region:
     raise ValueError("Environment variables CDK_DEFAULT_ACCOUNT and CDK_DEFAULT_REGION must be set")
